a289443b639db39a35d76a10e80cbe33430921b2
Atul Bhardwaj <atulmncfc@gmail.com>
2025-09-12 15:02:48 +0530

    added some chnges

--- mapvu/common.py
+++ mapvu/common.py
@@ -5,3 +5,11 @@ def hello_world():
     """Prints "Hello World!" to the console.
     """
     print("Hello World!")
+
+def greetings(name):
+    """Greets the name by user.
+
+    Args:
+        name (str): The name of the user.
+    """
+    print(f"Hello, {name}")    
